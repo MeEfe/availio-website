4eb9b847e51fb8fe219d30693f30d3c2e1b1ad5c
EfeErter <efdal.erter@gmail.com>
2025-09-23 18:42:57 +0200

    Testimonials page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Footer from "./components/ui/footer";
 import Header from "./components/ui/header";
 import LandingPage from "./pages/landing-page";
 import ThreeCursor from "./components/ui/three-cursor";
+import { Testimonials } from "./components/ui/testimonials";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <ThreeCursor />
       <Header />
       <LandingPage />
+      <Testimonials />
       <Footer />
     </>
   );
